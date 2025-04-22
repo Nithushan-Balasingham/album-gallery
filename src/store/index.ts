f24097c34c7012f0641709cd6bf3068eb0a40804
Nithushan-Balasingham <nithushanbalasingham@gmail.com>
2025-04-22 17:09:54 +0530

    clean

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,39 +2,32 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import albumReducer from './slices/albumSlice'; // Make sure the album slice is set up properly
+import albumReducer from './slices/albumSlice'; 
 
-// Persist configuration: specifies the slices to persist
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['album', 'search'], // Specify which slices to persist (album data, search term, etc.)
+  whitelist: ['album', 'photos'], 
 };
 
-// Combine all reducers (in case you have more slices, you can add them here)
 const rootReducer = combineReducers({
-  album: albumReducer, // Note: 'album' is the key for this slice
+  album: albumReducer, 
 });
 
-// Apply Redux Persist to root reducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// Create the store with the persisted reducer
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false, // Avoid errors when working with non-serializable data
+      serializableCheck: false,
     }),
 });
 
-// Create a persistor (responsible for storing the persisted state in storage)
 export const persistor = persistStore(store);
 
-// Typed hooks for accessing state and dispatch
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-// Custom hooks for dispatch and selector
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/store/slices/albumSlice.ts
+++ src/store/slices/albumSlice.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-// Define initial state for the album slice
 interface AlbumState {
   selectedAlbum: any | null;
   collections: any[];
@@ -15,35 +14,28 @@ const initialState: AlbumState = {
   page: 1,
 };
 
-// Create album slice
 const albumSlice = createSlice({
   name: 'album',
   initialState,
   reducers: {
-    // Action to set the selected album
     setSelectedAlbum(state, action: PayloadAction<any>) {
       state.selectedAlbum = action.payload;
     },
-    // Action to set the list of collections
     setCollections(state, action: PayloadAction<any[]>) {
       state.collections = action.payload;
     },
-    // Action to set the search term
     setSearch(state, action: PayloadAction<string>) {
       state.search = action.payload;
     },
-    // Action to set the page number
     setPage(state, action: PayloadAction<number>) {
       state.page = action.payload;
     },
-    // Clear the selected album
     clearSelectedAlbum(state) {
       state.selectedAlbum = null;
     },
   },
 });
 
-// Export the actions for use in components
 export const { 
   setSelectedAlbum, 
   setCollections, 
@@ -52,5 +44,4 @@ export const {
   clearSelectedAlbum 
 } = albumSlice.actions;
 
-// Export the reducer to be used in the store
 export default albumSlice.reducer;
